4b6814f61171647a5305a0d8820e61b6101ab2d2
mdraselmiah95 <russelroy15@gmail.com>
2022-11-26 18:28:12 +0600

    Learning NodeJs

---
+++ Node-Package-Manager-(NPM)/NPM/index.js
@@ -0,0 +1,6 @@
+// const chalk = require("chalk");
+const id = require("uuid");
+const c = require("ansi-colors");
+
+console.log(c.blueBright("This is true"));
+console.log(c.red.bold.underline("this is a bold green underlined message"));
