38bffb23a1a142ae3809d2dfe4ee8f856bb11a9e
mdraselmiah95 <russelroy15@gmail.com>
2022-11-26 16:43:28 +0600

    Learning NodeJs

--- NodeJS-Module-System/path.js
+++ NodeJS-Module-System/path.js
@@ -5,3 +5,12 @@ const path = require("path");
 const myCurrentPath = __filename;
 
 console.log(path.dirname(__dirname));
+console.log(path.extname(myCurrentPath));
+
+let pathObj = {
+  dir: "usr/local",
+  name: "testFile",
+  ext: ".js",
+};
+
+console.log(path.format(pathObj));
