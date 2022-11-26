a4637dc18ea71723e845629bd7eec88c3079c0f6
mdraselmiah95 <russelroy15@gmail.com>
2022-11-26 16:55:11 +0600

    Learning NodeJs

--- NodeJS-Module-System/os.js
+++ NodeJS-Module-System/os.js
@@ -6,3 +6,9 @@ console.log(os.arch());
 console.log(os.cpus());
 console.log(os.freemem());
 console.log(os.networkInterfaces());
+
+console.log(os.hostname());
+console.log(os.homedir());
+console.log(os.type());
+console.log(os.userInfo());
+console.log(os.uptime());
