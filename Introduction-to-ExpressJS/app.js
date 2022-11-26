915090245f15bdddaf8aeb0210a4a14d3123c8eb
mdraselmiah95 <russelroy15@gmail.com>
2022-11-27 00:52:23 +0600

    Learning NodeJS Dive into ExpressJS

--- Introduction-to-ExpressJS/app.js
+++ Introduction-to-ExpressJS/app.js
@@ -4,7 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 4040;
 
 app.get("/about", (req, res) => {
-  res.send(`<h3>About Route is Running 🏺</h3>`);
+  // res.send(`<h3>About Route is Running 🏺</h3>`);
+  res.json({
+    message: "I am a json Page",
+  });
 });
 
 app.get("/help", (req, res) => {
@@ -22,3 +25,5 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
   console.log(`App is Running on PORT ${PORT}`);
 });
+
+// this is cool
