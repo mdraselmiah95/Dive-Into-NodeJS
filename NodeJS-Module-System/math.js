1d93583a5822dd584336c0cce8875750dfce929e
mdraselmiah95 <russelroy15@gmail.com>
2022-11-26 16:11:07 +0600

    Learning NodeJs

--- NodeJS-Module-System/math.js
+++ NodeJS-Module-System/math.js
@@ -1,9 +1,15 @@
-const add = (a, b) => a + b;
+module.exports.add = (a, b) => a + b;
 const sub = (a, b) => a - b;
 const div = (a, b) => a / b;
 
 const test = (a, b) => add(a, b) / sub(a, b);
 
-module.exports = test;
+// module.exports = {
+//   test,
+//   div,
+// };
+
+// module.exports.add = add;
+// module.exports.test = test;
 
 console.log(module);
