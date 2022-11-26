bcc291d33a845c0840186118356e5821ef90aff4
mdraselmiah95 <russelroy15@gmail.com>
2022-11-27 02:02:10 +0600

    Learning NodeJS Dive into ExpressJS

--- Working-with-Express-Middleware/index.js
+++ Working-with-Express-Middleware/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+app.use(morgan("dev"));
 const PORT = process.env.PORT || 4040;
+
+app.get("/about", (req, res) => {
+  res.send(`<h3>This About Page is Running 🔰</h3>`);
+});
 
 app.get("/", (req, res) => {
   res.send("Server is Running 🦦");
