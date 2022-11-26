529c3dc9c3a22751389ba4e61f3b3a9b725ca6a1
mdraselmiah95 <russelroy15@gmail.com>
2022-11-26 18:35:30 +0600

    Learning NodeJs

--- Node-Package-Manager-(NPM)/NPM/index.js
+++ Node-Package-Manager-(NPM)/NPM/index.js
@@ -4,3 +4,5 @@ const c = require("ansi-colors");
 
 console.log(c.blueBright("This is true"));
 console.log(c.red.bold.underline("this is a bold green underlined message"));
+console.log(c.red("I am cool color."));
+console.log(9090);
