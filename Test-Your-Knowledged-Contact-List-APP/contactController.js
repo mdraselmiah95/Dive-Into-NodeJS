43552efb3be1746c5abec2f8cfdb883b53c787d3
mdraselmiah95 <russelroy15@gmail.com>
2022-11-27 20:09:05 +0600

    Learning NodeJS on ExpressJS

--- Test-Your-Knowledged-Contact-List-APP/contactController.js
+++ Test-Your-Knowledged-Contact-List-APP/contactController.js
@@ -5,3 +5,15 @@ exports.getAllContact = (req, res) => {
 exports.getSingleContact = (req, res) => {
   res.send("I am Contact" + req.params.contId);
 };
+
+exports.createNewContact = (req, res) => {
+  res.send("Create New Contact 🖱️");
+};
+
+exports.updateContact = (req, res) => {
+  res.send("Update Your Existing Contact 🚧" + req.params.contId);
+};
+
+exports.deleteContact = (req, res) => {
+  res.send("Delete Your Existing Contact 🏚️" + req.params.contId);
+};
--- Test-Your-Knowledged-Contact-List-APP/contactRoute.js
+++ Test-Your-Knowledged-Contact-List-APP/contactRoute.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
 
-const { getAllContact, getSingleContact } = require("./contactController");
+const {
+  getAllContact,
+  getSingleContact,
+  createNewContact,
+  updateContact,
+  deleteContact,
+} = require("./contactController");
 
 router.get("/", getAllContact);
 
 router.get("/:contId", getSingleContact);
+
+router.post("/", createNewContact);
+
+router.put("/:contId", updateContact);
+
+router.delete("/:contId", deleteContact);
 
 module.exports = router;
