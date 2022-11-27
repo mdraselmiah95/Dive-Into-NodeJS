1a4edac175087053eeadd811cbbb1833d3fc2035
mdraselmiah95 <russelroy15@gmail.com>
2022-11-27 16:45:07 +0600

    Learning NodeJS ExpressJS

--- Working-with-Express-Middleware/index.js
+++ Working-with-Express-Middleware/index.js
@@ -5,6 +5,13 @@ const app = express();
 
 // app.use(morgan("dev"));  => middleware
 
+function customMiddleware(req, res, next) {
+  console.log("I am Logged 🧘");
+  next();
+}
+
+app.use(customMiddleware);
+
 const PORT = process.env.PORT || 4040;
 
 app.get("/about", morgan("dev"), (req, res) => {
