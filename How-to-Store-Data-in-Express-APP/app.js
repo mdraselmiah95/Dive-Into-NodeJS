2616168a27a832af7fc58a9a2c9edfc9cd190519
mdraselmiah95 <russelroy15@gmail.com>
2022-11-28 22:37:20 +0600

    Learning NodeJS Store Data

--- How-to-Store-Data-in-Express-APP/app.js
+++ How-to-Store-Data-in-Express-APP/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
+
 app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server is Running 🚤");
+  res.json({ message: "This is welcome message" });
 });
 
 const PORT = process.env.PORT || 4040;
