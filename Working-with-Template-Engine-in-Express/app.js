7dbe8632706524484bec449b2398e448a4269739
mdraselmiah95 <russelroy15@gmail.com>
2022-11-28 17:05:09 +0600

    Learning NodeJS on Template Engine

--- Working-with-Template-Engine-in-Express/app.js
+++ Working-with-Template-Engine-in-Express/app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server is Running 🏃");
+  res.render("index");
 });
 
 const PORT = process.env.PORT || 4040;
