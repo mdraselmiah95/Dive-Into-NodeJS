f27d1e9c9e8b8dbc7dff7a65d5792e3ed31ebab6
mdraselmiah95 <russelroy15@gmail.com>
2022-11-29 00:21:47 +0600

    Learning NodeJS Connecting DataBase

--- How-to-Store-Data-in-Express-APP/controllers.js
+++ How-to-Store-Data-in-Express-APP/controllers.js
@@ -1,8 +1,51 @@
-exports.getAllContact = (req, res) => {};
+const Contact = require("./Contact");
 
-exports.getSingleContact = (req, res) => {};
+exports.getAllContact = (req, res) => {
+  Contact.find()
+    .then((contacts) => {
+      res.json(contacts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.json({
+        message: "Error Occurred 💥",
+      });
+    });
+};
 
-exports.createContact = (req, res) => {};
+exports.getSingleContact = (req, res) => {
+  let { id } = req.params;
+  Contact.findById(id)
+    .then((contact) => {
+      res.json(contact);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.json({
+        message: "Error Occurred 💥",
+      });
+    });
+};
+
+exports.createContact = (req, res) => {
+  let { name, phone, email } = req.body;
+  let contact = new Contact({
+    name,
+    email,
+    phone,
+  });
+  contact
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.json({
+        message: "Error Occurred 💥",
+      });
+    });
+};
 
 exports.updateContact = (req, res) => {};
 
