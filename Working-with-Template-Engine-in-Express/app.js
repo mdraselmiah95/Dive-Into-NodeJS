ba76f478bdae4672e2b0ff5f1c467d6b9441a1fe
mdraselmiah95 <russelroy15@gmail.com>
2022-11-28 16:57:32 +0600

    Learning NodeJS on Template Engine

--- Working-with-Template-Engine-in-Express/app.js
+++ Working-with-Template-Engine-in-Express/app.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
