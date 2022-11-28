2e47976607f5918de8385f9e0341c15f13d56123
mdraselmiah95 <russelroy15@gmail.com>
2022-11-28 23:38:09 +0600

    Learning NodeJS Connecting DataBase

--- How-to-Store-Data-in-Express-APP/app.js
+++ How-to-Store-Data-in-Express-APP/app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
+const router = require("./routes");
+
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use("/contacts", router);
 
 app.get("/", (req, res) => {
   res.send("");
