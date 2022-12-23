0b59ebb08beb151dafbaf5cf2a45693b6df6a235
mdraselmiah95 <russelroy15@gmail.com>
2022-12-23 18:57:48 +0600

    WIP on Final project Schema

--- Final-project/models/Profile.js
+++ Final-project/models/Profile.js
@@ -2,27 +2,45 @@
 
 const { Schema, model } = require("mongoose");
 
-const profileSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    require: true,
+const profileSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    title: {
+      type: String,
+      trim: true,
+      maxlength: 100,
+    },
+    bio: {
+      type: String,
+      trim: true,
+      maxlength: 500,
+    },
+    profilePic: String,
+    links: {
+      website: String,
+      facebook: String,
+      twitter: String,
+      github: String,
+    },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
   },
-  title: {
-    type: String,
-    trim: true,
-    maxlength: 100,
-  },
-  bio: {
-    type: String,
-    trim: true,
-    maxlength: 500,
-  },
-  profilePic: String,
-  links: {
-    website: String,
-    facebook: String,
-    twitter: String,
-    github: String,
-  },
-});
+  { timestamps: true }
+);
+
+const Profile = model("Profile", profileSchema);
+module.exports = Profile;
