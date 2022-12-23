16475828f5aaa21135100ccadff1dc01aa7d0a20
mdraselmiah95 <russelroy15@gmail.com>
2022-12-24 01:44:04 +0600

    WIP on Final project form

--- Final-project/models/Profile.js
+++ Final-project/models/Profile.js
@@ -11,6 +11,12 @@ const profileSchema = new Schema(
       ref: User,
       required: true,
     },
+    name: {
+      type: String,
+      trim: true,
+      maxlength: 20,
+      required: true,
+    },
     title: {
       type: String,
       trim: true,
