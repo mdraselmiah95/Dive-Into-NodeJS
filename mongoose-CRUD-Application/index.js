d20f55692f174ad2dc0071a0394c1b68092280b6
mdraselmiah95 <russelroy15@gmail.com>
2022-12-24 23:49:36 +0600

    Mongoose CRUD Application setup route

--- mongoose-CRUD-Application/index.js
+++ mongoose-CRUD-Application/index.js
@@ -1,27 +1,36 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 mongoose.set("strictQuery", false);
 const todoHandler = require("./routes/todoHandler");
 
 // TODO: express application initialization
-const app = express();
-app.use(express.json());
+const middleware = [express.urlencoded({ extended: true }), express.json()];
+const app = express(middleware);
 
 //TODO: database connection with mongoose
+const PORT = process.env.PORT || 5000;
 mongoose
-  .connect("mongodb://localhost/todos", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dkgxs.mongodb.net/?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("DATABASE CONNECTED...");
+    app.listen(PORT, () => {
+      console.log(`App is Running on PORT ${PORT}`);
+    });
   })
-  .then(() => console.log("connection successful"))
-  .catch((err) => console.log(err));
+  .catch((error) => {
+    return console.log(error);
+  });
+
+//TODO: application routes
+app.use("/todo", todoHandler);
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-
-//TODO: application routes
-app.use("/todo", todoHandler);
 
 //TODO: default error handler
 function errorHandler(err, req, res, next) {
@@ -31,6 +40,6 @@ function errorHandler(err, req, res, next) {
   res.status(500).json({ error: err });
 }
 
-app.listen(3000, () => {
-  console.log("app listening at port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("app listening at port 3000");
+// });
