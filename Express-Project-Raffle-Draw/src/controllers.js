424f39be9b5abe334d0e2c83b1572fbf954f76e0
mdraselmiah95 <russelroy15@gmail.com>
2022-12-26 02:10:27 +0600

    WIP on Raffle Draw Project

--- Express-Project-Raffle-Draw/src/controllers.js
+++ Express-Project-Raffle-Draw/src/controllers.js
@@ -2,6 +2,20 @@ const ticketCollection = require("./tickets");
 
 // TODO: ticket selling controllers
 
-exports.sellSingleTicket = (req, res, next) => {};
+exports.sellSingleTicket = (req, res, next) => {
+  const { username, price } = req.body;
+  const ticket = ticketCollection.create(username, price);
+  res.status(201).json({
+    message: "Ticket created successfully 🟢",
+    ticket,
+  });
+};
 
-exports.sellBulkTicket = (req, res, next) => {};
+exports.sellBulkTicket = (req, res, next) => {
+  const { username, price, quantity } = req.body;
+  const tickets = ticketCollection.createBulk(username, price, quantity);
+  res.status(201).json({
+    message: "Tickets creates successfully 🟢",
+    tickets,
+  });
+};
