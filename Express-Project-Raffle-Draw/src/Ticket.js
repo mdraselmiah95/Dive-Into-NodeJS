735ab9bdefb0bded3352ecce3264390fd4cb6722
mdraselmiah95 <russelroy15@gmail.com>
2022-12-25 22:40:47 +0600

    WIP on Raffle Draw Project

--- Express-Project-Raffle-Draw/src/Ticket.js
+++ Express-Project-Raffle-Draw/src/Ticket.js
@@ -1,6 +1,11 @@
 const shortid = require("shortid");
 
 class Ticket {
+  /**
+   * Ticket constructor will receive username and price
+   * @param {string} username
+   * @param {number} price
+   */
   constructor(username, price) {
     this.id = shortid.generate();
     this.username = username;
@@ -9,3 +14,5 @@ class Ticket {
     this.updateAt = new Date();
   }
 }
+
+module.exports = Ticket;
