f5af1cff77a8ddf775457a7fdb3c181ad6fb9ff6
mdraselmiah95 <russelroy15@gmail.com>
2023-01-29 01:42:56 +0600

    Final Project adding Validator

--- Final-project/routes/authRoute.js
+++ Final-project/routes/authRoute.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const { body } = require("express-validator");
+const User = require("../models/User");
 
 const {
   signUpGetController,
@@ -7,6 +9,27 @@ const {
   loginPostController,
   logoutController,
 } = require("../controllers/authController");
+
+const signupValidator = [
+  body("username")
+    .isLength({ min: 2, max: 15 })
+    .withMessage("Username Must Be Between 2 to 15 Chars")
+    .custom(async (username) => {
+      let user = await User.findOne({ username });
+      if (user) {
+        return Promise.reject("Username Already Used");
+      }
+    }),
+  body("email")
+    .isEmail()
+    .withMessage("Please Provide a Valid Email")
+    .custom(async (email) => {
+      let user = await User.findOne({ email });
+      if (user) {
+        return Promise.reject("Email Already Used");
+      }
+    }),
+];
 
 router.get("/signup", signUpGetController);
 router.post("/signup", signUpPostController);
