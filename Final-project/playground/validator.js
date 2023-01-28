084ddd9fd30c5d35cdd87d0d36567c320b686e20
mdraselmiah95 <russelroy15@gmail.com>
2023-01-28 19:45:07 +0600

    Final Project validator

--- Final-project/playground/validator.js
+++ Final-project/playground/validator.js
@@ -1,9 +1,25 @@
 const router = require("express").Router();
+const { check, validationResult } = require("express-validator");
 
 router.get("/validator", (req, res, next) => {
   res.render("playground/signup", { title: "Validator Playground" });
 });
 
-router.post("/validator", (req, res, next) => {});
+router.post(
+  "/validator",
+  [
+    check("username")
+      .not()
+      .isEmpty()
+      .isLength({ max: 15 })
+      .withMessage(`UserName can not be grater than 15 Character`),
+    check("email").isEmail().withMessage("Please Provide a Valid Email 🌴"),
+  ],
+  (req, res, next) => {
+    let error = validationResult(req);
+    console.log(error);
+    res.render("playground/signup", { title: "Validator Playground" });
+  }
+);
 
 module.exports = router;
