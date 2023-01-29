7a5045a1396b5a86c28b4785c33d9f7c4df64a44
mdraselmiah95 <russelroy15@gmail.com>
2023-01-29 17:24:28 +0600

    Final Project Validation

--- Final-project/controllers/authController.js
+++ Final-project/controllers/authController.js
@@ -46,10 +46,18 @@ exports.signUpPostController = async (req, res, next) => {
 };
 
 exports.loginGetController = (req, res, next) => {
-  res.render("pages/auth/login", { title: "Login To Your Account" });
+  res.render("pages/auth/login", { title: "Login To Your Account", error: {} });
 };
 exports.loginPostController = async (req, res, next) => {
   let { email, password } = req.body;
+
+  let errors = validationResult(req).formatWith(errorFormatter);
+  if (!errors.isEmpty()) {
+    return res.render("pages/auth/login", {
+      title: "Login To Your Account",
+      error: errors.mapped(),
+    });
+  }
 
   try {
     let user = await User.findOne({ email });
--- Final-project/routes/authRoute.js
+++ Final-project/routes/authRoute.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const signupValidator = require("../validator/auth/signupValidator");
+const loginValidator = require("../validator/auth/loginValidator");
 
 const {
   signUpGetController,
@@ -13,7 +14,7 @@ router.get("/signup", signUpGetController);
 router.post("/signup", signupValidator, signUpPostController);
 
 router.get("/login", loginGetController);
-router.post("/login", loginPostController);
+router.post("/login", loginValidator, loginPostController);
 
 router.get("/logout", logoutController);
 
