e010889eda819250240f6e4459e52ba87cc78280
mdraselmiah95 <russelroy15@gmail.com>
2023-03-16 22:34:42 +0600

    Express JS

--- NodeJS-Express-MongoDB-Master-🔥/Node-3/index.js
+++ NodeJS-Express-MongoDB-Master-🔥/Node-3/index.js
@@ -1,14 +1,34 @@
 const fs = require("fs");
 const express = require("express");
 // const index = fs.readdirSync("index.html", "utf-8");
-// const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-// const products = data.products;
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.products;
 
 const app = express();
 const PORT = 3000;
 
-app.use("/", (req, res) => {
-  res.send("Hello world");
+app.post("/", (req, res) => {
+  res.json({ type: "POST" });
+});
+
+app.put("/", (req, res) => {
+  res.json({ type: "PUT" });
+});
+
+app.patch("/", (req, res) => {
+  res.json({ type: "PATCH" });
+});
+
+app.delete("/", (req, res) => {
+  res.json({ type: "DELETE" });
+});
+
+app.get("/demo", (req, res) => {
+  // res.sendStatus(404)
+  //   res.json(products);
+  //   res.send("Hello world");
+  res.status(200).send("Hello World");
+  //   res.sendFile("");
 });
 
 app.listen(PORT, () => {
