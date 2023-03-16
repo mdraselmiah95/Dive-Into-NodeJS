c42a0c29c8cb9ebb0e3bea623b476423e9d00d24
mdraselmiah95 <russelroy15@gmail.com>
2023-03-16 17:16:14 +0600

    Server Concepts with Node - http module

--- NodeJS-Express-MongoDB-Master-🔥/Node-2/index.js
+++ NodeJS-Express-MongoDB-Master-🔥/Node-2/index.js
@@ -1,7 +1,16 @@
 const http = require("http");
+const fs = require("fs");
 
+// const index = fs.readFileSync("index.html", "utf-8");
+const index = fs.readFileSync("data.json", "utf-8");
+
+const data = { age: 24 };
 const server = http.createServer((req, res) => {
+  console.log(req.url);
   console.log("SERVER IS RUNNING");
+  res.setHeader("Content-Type", "Application/json");
+  //   res.setHeader("Content-Type", "text/html");
+  res.end(index);
 });
 
 server.listen(3000);
