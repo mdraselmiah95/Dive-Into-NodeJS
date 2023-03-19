6439a4a844d925c6cab3b29cc0ba0fe50f6645b6
mdraselmiah95 <russelroy15@gmail.com>
2023-03-20 05:37:01 +0600

    REST APIs and CRUD

---
+++ NodeJS-Express-MongoDB-Master-🔥/Node-4/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+const PORT = 3000;
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World 🏛️");
+});
+
+app.listen(PORT, () => {
+  console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+});
