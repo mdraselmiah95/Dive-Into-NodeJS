264dffa7ab5b409ab2abc9e6cdf7ba5fce71f10a
mdraselmiah95 <russelroy15@gmail.com>
2023-03-21 04:39:02 +0600

    Model View Controller (MVC)

--- NodeJS-Express-MongoDB-Master-🔥/Node-4/index.js
+++ NodeJS-Express-MongoDB-Master-🔥/Node-4/index.js
@@ -16,17 +16,63 @@ app.get("/", (req, res) => {
 
 // TODO: APT ROOT , Base URL, example - google.com/api/v2
 
-// Products
+const createProduct = (req, res) => {
+  products.push(req.body);
+  res.send({ message: "Data inserted Successfully" });
+};
 
-app.get("/products", (req, res) => {
+const getAllProduct = (req, res) => {
   res.json(products);
-});
+};
 
-app.get("/products/:id", (req, res) => {
+const getProductById = (req, res) => {
   const id = +req.params.id;
   const product = products.find((p) => p.id === id);
   res.json(product);
-});
+};
+
+const updateProductById = (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  products.splice(productIndex, 1, { ...req.body, id });
+  res.status(200).json();
+};
+
+const updateSingleProductById = (req, res) => {
+  const id = +req.params.id;
+  const index = products.findIndex((p) => p.id === id);
+  const product = products[index];
+  products.splice(index, 1, { ...product, ...req.body });
+  res.status(200).json();
+};
+
+const deleteProduct = (req, res) => {
+  const id = +req.params.id;
+  const index = products.findIndex((p) => p.id === id);
+  const product = products[index];
+  products.splice(index, 1);
+  res.status(200).json(product);
+};
+
+// Products
+
+// Create POST/products             C R U D
+app.post("/products", createProduct);
+
+// Read All Product GET
+app.get("/products", getAllProduct);
+
+// Read GET /products/:id
+app.get("/products/:id", getProductById);
+
+// Update PUT /products/:id all update
+app.put("/products/:id", updateProductById);
+
+// Update PATCH /products/:id single item update
+app.patch("/products/:id", updateSingleProductById);
+
+// DELETE /products/:id
+app.delete("/products/:id", deleteProduct);
 
 app.listen(PORT, () => {
   console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
