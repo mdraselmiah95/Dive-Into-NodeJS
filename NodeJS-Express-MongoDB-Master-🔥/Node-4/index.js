9e9771c8d245cd72e1b569e0d033d02a22352a37
mdraselmiah95 <russelroy15@gmail.com>
2023-03-20 06:33:22 +0600

    REST APIs and CRUD

--- NodeJS-Express-MongoDB-Master-🔥/Node-4/index.js
+++ NodeJS-Express-MongoDB-Master-🔥/Node-4/index.js
@@ -1,13 +1,23 @@
+const fs = require("fs");
 const express = require("express");
 const morgan = require("morgan");
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.products;
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World 🏛️");
+});
+
+// Products
+
+app.get("/products", (req, res) => {
+  res.json(products);
 });
 
 app.listen(PORT, () => {
