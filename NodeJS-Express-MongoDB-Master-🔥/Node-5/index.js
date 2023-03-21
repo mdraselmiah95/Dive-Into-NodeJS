ca987739b7606978e16add6d28c871e33d94b75a
mdraselmiah95 <russelroy15@gmail.com>
2023-03-22 01:03:43 +0600

    Mongoose and REST APIs

--- NodeJS-Express-MongoDB-Master-🔥/Node-5/index.js
+++ NodeJS-Express-MongoDB-Master-🔥/Node-5/index.js
@@ -1,9 +1,34 @@
 require("dotenv").config();
-const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const express = require("express");
+const { Schema } = mongoose;
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// DB connection
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/e-com");
+  console.log("Database Connect");
+}
+
+// Schema
+const productSchema = new Schema({
+  title: String,
+  description: String,
+  price: Number,
+  discountPercentage: Number,
+  rating: Number,
+  brand: String,
+  category: String,
+  thumbnail: String,
+  images: [String],
+});
+
+const Product = mongoose.model("Product", productSchema);
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -12,7 +37,7 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-// Route the will come findOne {} and fine []
+// Route
 
 app.listen(process.env.PORT, () => {
   console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
