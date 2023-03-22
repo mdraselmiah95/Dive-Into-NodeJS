b4db938ec16e5d1f247f880f61ace380abe7bf94
mdraselmiah95 <russelroy15@gmail.com>
2023-03-23 02:12:04 +0600

    React Integration and MERN Stack Live deployment

--- NodeJS-Express-MongoDB-Master-🔥/Node-6/front-end/src/components/Home.js
+++ NodeJS-Express-MongoDB-Master-🔥/Node-6/front-end/src/components/Home.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
-import ProductData from "../../public/data.json";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Product from "./Product";
 
 const Home = () => {
-  const [products, setProducts] = useState(ProductData);
+  const [products, setProducts] = useState();
+
+  const getProducts = async () => {
+    const data = await axios.get("http://localhost:8080/api/v1/products");
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
 
   return (
     <div>
-      {products.map((product, index) => (
-        <Product product={product} key={index} />
-      ))}
+      <Product />
     </div>
   );
 };
