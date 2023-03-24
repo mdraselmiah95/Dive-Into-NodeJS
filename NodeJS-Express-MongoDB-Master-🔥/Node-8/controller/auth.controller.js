56fd7f0316b75e041acb82f9cea18741fb929268
mdraselmiah95 <russelroy15@gmail.com>
2023-03-25 05:17:11 +0600

    Signing of JWT Token

--- NodeJS-Express-MongoDB-Master-🔥/Node-8/controller/auth.controller.js
+++ NodeJS-Express-MongoDB-Master-🔥/Node-8/controller/auth.controller.js
@@ -1,7 +1,9 @@
 const User = require("../model/user.model");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const fs = require("fs");
 const path = require("path");
+const { token } = require("morgan");
 const privateKey = fs.readFileSync(
   path.resolve(__dirname, "../private.key"),
   "utf-8"
@@ -12,6 +14,8 @@ exports.signUp = async (req, res) => {
     const token = jwt.sign({ email: req.body.email }, privateKey, {
       algorithm: "RS256",
     });
+    const hash = bcrypt.hashSync(req.body.password, 10);
+    user.password = hash;
     user.token = token;
     user.save();
     res.status(200).json({
@@ -22,6 +26,26 @@ exports.signUp = async (req, res) => {
     res.status(400).json({
       status: "Fail",
       error: "Couldn't create the User",
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const doc = await User.findOne({ email: req.body.email });
+    const isAuth = bcrypt.compareSync(req.body.password, doc.password);
+    if (isAuth) {
+      let token = jwt.sign({ email: req.body.email }, privateKey, {
+        algorithm: "RS256",
+      });
+      res.json({ token });
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (error) {
+    res.status(401).json({
+      status: "Unauthorized ",
+      error: "The server's request was not verified 🔥",
     });
   }
 };
--- NodeJS-Express-MongoDB-Master-🔥/Node-8/routes/auth.route.js
+++ NodeJS-Express-MongoDB-Master-🔥/Node-8/routes/auth.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controller/auth.controller");
 
-router.route("/").post(authController.signUp);
+router.route("/signup").post(authController.signUp);
+router.route("/login").post(authController.login);
 
 module.exports = router;
